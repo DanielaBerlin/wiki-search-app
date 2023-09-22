446a005e3c68e30c6429a258a950fe378487a0d3
Daniela <danielacardona2203@gmail.com>
2023-09-22 12:17:23 -0500

    Refactor displayResults function for improved readability and code structure

--- script.js
+++ script.js
@@ -10,30 +10,51 @@ const apiURL =
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-    const searchValue = search.value;
-    if (searchValue === "") {
-        errorMessage("Search cannot be empty, please enter a search term.")
-    } else {
-        getResult(searchValue);
-    }
+  const searchValue = search.value;
+  if (searchValue === '') {
+    errorMessage('Search cannot be empty, please enter a search term.');
+  } else {
+    getResult(searchValue);
+  }
 });
 
 //Error Message
 function errorMessage(msg) {
-    errorMsg.style.display = "block";
-    line.style.display = "block";
-    errorMsg.textContent = msg;
+  errorMsg.style.display = 'block';
+  line.style.display = 'block';
+  errorMsg.textContent = msg;
 }
 
-
 async function getResult(searchVal) {
-    const response = await fetch(apiURL + searchVal);
-    const results = await response.json();
+  const response = await fetch(apiURL + searchVal);
+  const results = await response.json();
 
-    console.log(results);
-    if (results.query.search.length == 0) {
-        return errorMessage("Invalid search, please enter another search term.");
-    } else {
-        displayResults(results);
-    }
+  console.log(results);
+  if (results.query.search.length == 0) {
+    return errorMessage('Invalid search, please enter another search term.');
+  } else {
+    displayResults(results);
+  }
+}
+
+//Display results
+function displayResults(results) {
+  line.style.display = 'block';
+  let output = '';
+  results.query.search.forEach((result) => {
+    let resultURL = `https://en.wikipedia.org/?curid=${result.pageid}`;
+    output += `
+        <div class="results p-2">
+      <a href="${resultURL}" target="_blank" rel="noopener" class="h3 fw-bold">${result.title}</a>
+      <br />
+      <a href="${resultURL}" target="_blank" rel="noopener" class="fs-5 text-success"
+        >${resultURL}</a
+      >
+      <p class="fs-5">
+${result.snippet}
+      </p>
+    </div>
+        `;
+    searchResults.innerHTML = output;
+  });
 }
